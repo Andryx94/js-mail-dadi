952d5a22eb44f3133284f1da751918667fa2718d
Andrea <andryx1994@gmail.com>
2020-05-26 10:24:46 +0200

    Versione 1.7
    Miglioramenti grafici, inserito alert aggiunta email,  rivisto visualizzazione risultati dadi, sistemato codice html, css e js

--- js/script.js
+++ js/script.js
@@ -1,42 +1,31 @@
 //CONTROLLO EMAIL
-// variabili verifica Email
-var emailAggiungi = document.getElementById('email-aggiungi');
-var bottoneInviaAggiungi = document.getElementById('bottone-invia-aggiungi');
-var bottoneAnnullaAggiungi = document.getElementById('bottone-annulla-aggiungi');
-var emailValidaAggiuntaLista = document.getElementById('email-valida-aggiunta');
-var emailValide = ["nomecognome@gmail.com", "nomecognome@yahoo.com", "nomecognome@libero.it", "nomecognome@alice.it"]
-
 //variabili convalida Email
 var emailVerifica = document.getElementById('email-verifica');
 var bottoneInviaAVerifica = document.getElementById('bottone-invia-verifica');
 var bottoneAnnullaVerifica = document.getElementById('bottone-annulla-verifica');
 var alertEmailVerifica = document.getElementById('alert-email-verifica');
 
-//click verifica email
-bottoneInviaAggiungi.addEventListener('click',
-function () {
-  var emailValidaAggiunta = emailAggiungi.value
-  emailValide.push(emailValidaAggiunta);
-  emailValidaAggiuntaLista.innerHTML = emailValidaAggiunta;
-}
-);
+// variabili verifica Email
+var emailAggiungi = document.getElementById('email-aggiungi');
+var bottoneInviaAggiungi = document.getElementById('bottone-invia-aggiungi');
+var bottoneAnnullaAggiungi = document.getElementById('bottone-annulla-aggiungi');
+var alertEmailAggiungi = document.getElementById('alert-email-aggiungi');
 
-bottoneAnnullaAggiungi.addEventListener('click',
-function () {
-  emailAggiungi.innerHTML = emailAggiungi.value = "";
-}
-);
+//variabile array email email valide
+var emailValide = ["nomecognome@gmail.com", "nomecognome@yahoo.com", "nomecognome@libero.it", "nomecognome@alice.it"]
 
 //click convalida email
 bottoneInviaAVerifica.addEventListener('click',
 function () {
-  if (alertEmailVerifica.value != "L'Email è presente") {
-    alertEmailVerifica.innerHTML = "L'Email non è presente";
-  }
+  if (emailVerifica.value != "") {
+    if (alertEmailVerifica.value != "L'Email è presente") {
+      alertEmailVerifica.innerHTML = "L'Email non è presente";
+    }
 
-  for (var i=0; i< emailValide.length; i++) {
-    if (emailValide[i] == emailVerifica.value) {
-      alertEmailVerifica.innerHTML = "L'Email è presente";
+    for (var i=0; i< emailValide.length; i++) {
+      if (emailValide[i] == emailVerifica.value) {
+        alertEmailVerifica.innerHTML = "L'Email è presente";
+      }
     }
   }
 }
@@ -49,12 +38,33 @@ function () {
 }
 );
 
+//click verifica email
+bottoneInviaAggiungi.addEventListener('click',
+function () {
+  if (emailAggiungi.value != "") {
+    var emailValidaAggiunta = emailAggiungi.value
+    emailValide.push(emailValidaAggiunta);
+    alertEmailAggiungi.innerHTML = "Email aggiunta";
+  }
+}
+);
+
+bottoneAnnullaAggiungi.addEventListener('click',
+function () {
+  emailAggiungi.innerHTML = emailAggiungi.value = "";
+  alertEmailAggiungi.innerHTML = "";
+}
+);
+
+
 //DADI
 //variabili lancio dadi
 var bottoneDadoUtente = document.getElementById('bottone-dado-utente');
+var bottoneDadoAnnulla = document.getElementById('bottone-dado-annulla');
 var risultatoDadoUtente = document.getElementById('risultato-dado-utente');
 var risultatoDadoPC = document.getElementById('risultato-dado-pc');
 var vincitore = document.getElementById('vincitore');
+var noHidden = document.getElementById('nohidden');
 
 //click lancio dadi
 bottoneDadoUtente.addEventListener('click',
@@ -65,15 +75,23 @@ function () {
   risultatoDadoPC.innerHTML = dadoPC;
 
   if (dadoUtente > dadoPC) {
-    vincitore.innerHTML = "Hai vinto!"
+    vincitore.innerHTML = "Hai vinto! :)"
   }
 
   else if (dadoUtente < dadoPC) {
-    vincitore.innerHTML = "Hai perso :("
+    vincitore.innerHTML = "Hai perso! :("
   }
 
   else {
-    vincitore.innerHTML = "Pareggio!"
+    vincitore.innerHTML = "Pareggio! :|"
   }
+
+  noHidden.className = "";
+}
+);
+
+bottoneDadoAnnulla.addEventListener('click',
+function () {
+  noHidden.className = "hidden";
 }
 );
